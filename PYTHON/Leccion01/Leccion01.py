3c91ccc54487e4a9bb7633d1eb1b26176be1536e
MatiasMichaux <112663570+MatiasMichaux98@users.noreply.github.com>
2023-04-17 19:09:16 -0300

    Más de procedimientos de excepciones
    Código hecho en Python

--- PYTHON/Leccion01/Leccion01.py
+++ PYTHON/Leccion01/Leccion01.py
@@ -1,2 +1,13 @@
 #codigo de PYTHON
-#codigo de pyhton
+try :
+    a = int(input("digite un numero")) #agregamos input
+    b = int(input("digite otro numero"))
+    resultado = a / b
+except TypeError as e:
+    print(f"TypeError - Ha ocurrido un Error{type(e)} ")
+except ZeroDivisionError as e:
+    print(f"ZeroDivisionError -Ha ocurrido un ERROR {type(e)} ")
+except Exception as e:
+    print(f"Ocurrio un error : {e} ")
+
+print(f"El resultado es {resultado} ")
