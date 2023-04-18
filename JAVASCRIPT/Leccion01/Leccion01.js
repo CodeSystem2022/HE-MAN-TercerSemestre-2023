f00c0bfa96ad477bf6f5466d85204c8c429cd110
Mauricio Cerda <MauryCerda@users.github.com>
2023-04-18 20:09:45 -0300

    Break_Cerda_Maurcio
    Palabra_Reservada_Break

--- JAVASCRIPT/Leccion01/Leccion01.js
+++ JAVASCRIPT/Leccion01/Leccion01.js
@@ -1,1 +1,11 @@
 //codigo de javascript
+
+//Palabra reservada Continue
+for (let contando = 0; contando <=10; contando++){
+	if(contando % 2 !== 0){
+		continue; //Ir a la siguiente iteracion
+		
+	}
+	console.log(contando);  0,2,4,6,8,10
+}
+console.log("Termina el ciclo");
