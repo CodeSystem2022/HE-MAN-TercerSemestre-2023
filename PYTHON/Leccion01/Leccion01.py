936c9854d72cfb371de052a943c8c50de1091bd5
EMMANUEL-1985 <112596295+Emmanuel-1985@users.noreply.github.com>
2023-04-18 19:51:48 -0300

    Procesamiento de excepciones
    procesamiento de excepciones con python

--- PYTHON/Leccion01/Leccion01.py
+++ PYTHON/Leccion01/Leccion01.py
@@ -11,3 +11,15 @@ except Exception as e:
     print(f"Ocurrio un error : {e} ")
 
 print(f"El resultado es {resultado} ")
+
+# Procesamiento de excepciones
+resultado = None
+a = 10
+b = 0
+try :
+    resultado = a / b
+except Exception as e:
+    print(f"Ocurrio un error : {e} ")
+
+print(f"El resultado es {resultado} ")
+print("seguimos...")
