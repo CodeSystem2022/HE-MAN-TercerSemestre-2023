e0292a717692d765a99395238e64d5d6601179df
Luciano omar cortez <loc200381@gmail.com>
2023-04-19 20:27:23 -0300

    procesamiento de exepciones personalizadas
    procesamiento de exepciones personalizadas

--- PYTHON/Leccion01/Leccion01.py
+++ PYTHON/Leccion01/Leccion01.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-#codigo de PYTHON
-=======
 
->>>>>>> master
+#codigo de PYTHON
+
+
 try :
     a = int(input("digite un numero")) #agregamos input
     b = int(input("digite otro numero"))
@@ -15,7 +14,7 @@ except Exception as e:
     print(f"Ocurrio un error : {e} ")
 
 print(f"El resultado es {resultado} ")
-<<<<<<< HEAD
+
 
 # Procesamiento de excepciones
 resultado = None
@@ -28,5 +27,24 @@ except Exception as e:
 
 print(f"El resultado es {resultado} ")
 print("seguimos...")
-=======
->>>>>>> master
+
+# exepciones personalizadas en python
+try:
+     a = int(input(" digite el primer numero: "))
+     b = int(input(" digite el segundo numero: "))
+     if a == b:
+        raise NumerosIgualesException("Son numeros iguales") # la palabra raise nos permite arrojar cualquier excepcion
+     resultado = a / b # modificamos
+except TypeError as e:
+    print(f"TypeError - ocurrio un error: {type(e)}")
+except ZeroDivisionError as e:
+     print(f"ZeroDivisionError - ocurrio un error: {type(e)}")
+except Exception as e:
+      print(f"ocurrio un error: {type(e)}")
+  else:
+      print("No se arrojo ninguna excepcion")
+  finally: # siempre se va a ejecutar
+      print("ejecucion de este bloque finalizada")
+
+print(f"el resultado es: {resultado}")
+print("seguimos")
