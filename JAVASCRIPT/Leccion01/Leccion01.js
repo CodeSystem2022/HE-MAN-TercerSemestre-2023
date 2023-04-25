c4a049210492c80bcf2763be92859e1d36573838
MatiasMichaux <112663570+MatiasMichaux98@users.noreply.github.com>
2023-04-25 17:18:07 -0300

    Leccion1
    ciclos JS

--- JAVASCRIPT/Leccion01/Leccion01.js
+++ JAVASCRIPT/Leccion01/Leccion01.js
@@ -1,3 +1,27 @@
+//codigo de javascript
+// for
+
+for(let contando=0; contando < 3 ; contando++){
+    console.log(contando)
+}
+console.log('Fin del ciclo for')
+// while
+let contando = 0;
+while(contando < 3){
+    console.log(contando); 
+    contando++;
+}
+console.log("Fin del ciclo while"); 
+
+//do while
+let conteo = 0;
+do{
+    console.log(conteo); 
+    conteo++;
+}while(conteo < 3);
+
+console.log("Fin del ciclo do while");
+
 // Palabra reservada continue y Etiquetas Labels
 inicio:
 for(let contando = 0; contando <= 10; contando++){
