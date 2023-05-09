cc05a36c73d421525c1f32af7c0565d2c6577efc
2022sbm <112724907+2022sbm@users.noreply.github.com>
2023-05-09 20:09:50 -0300

    MundoPcJava
    MundoPcJava

---
+++ JAVA/Leccion01/MundoPcSebastianBarros/MundoPc/Mundo_PC/src/ar/com/system2023/mundopc/Computadora.java
@@ -0,0 +1,68 @@
+//Sebastian Barros
+
+package ar.com.system2023.mundopc;
+
+public class Computadora {
+    private final int idComputadora;
+    private String nombre;
+    private Teclado teclado;
+    private Monitor monitor;
+    private Raton raton;
+    private static int ContadorComputadora;
+    
+    //constructor vacio
+    Computadora(){
+        this.idComputadora= ++Computadora.ContadorComputadora;
+    }
+    //constructor II
+    Computadora(String nombre,Teclado teclado,Monitor monitor,Raton raton){
+        this();
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.raton = raton;
+        this.teclado = teclado;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Teclado getTeclado() {
+        return this.teclado;
+    }
+
+    public void setTeclado(Teclado teclado) {
+        this.teclado = teclado;
+    }
+
+    public Monitor getMonitor() {
+        return this.monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Raton getRaton() {
+        return this.raton;
+    }
+
+    public void setRaton(Raton raton) {
+        this.raton = raton;
+    }
+    
+    public int getidComputadora(){
+        return this.idComputadora;
+    }
+
+    @Override
+    public String toString() {
+        return "Computadora{" + "idComputadora=" + idComputadora + ", nombre=" + nombre + ", teclado=" + teclado + ", monitor=" + monitor + ", raton=" + raton + '}';
+    }
+    
+    
+}
