25cb0fac54210424fe8f933ff64b5b7bf22ecb4a
fabjav <112377555+fabjav@users.noreply.github.com>
2023-05-15 19:51:42 -0300

    Fabio Javier Flores
    concepto de hoisting

--- JAVASCRIPT/Leccion03/01-03-funciones.js
+++ JAVASCRIPT/Leccion03/01-03-funciones.js
@@ -51,14 +51,16 @@ console.log(resultado);
 // Sumar todo los argumentos
 let respuesta = sumarTodo(5, 4, 13, 10, 9);
 console.log(respuesta);
-function sumarTodo(){
+// Fabio Javier Flores - Concepto de Hoisting 
+//Sumar todo los argumentos
+let rta = SumarTodo(5,4,13,10,9);
+console.log(rta); 41
+function SumarTodo(){
     let suma = 0;
-    for(let i = 0; i < arguments.length; i++){
-        suma += arguments[i]; // arguments es para arreglos
+    for(let i = 0; i<arguments.length; i++){
+        suma += arguments[i];//'arguments' es para arreglos
     }
-    return suma;
 }
-
 //Tipos Primitivos
 let k = 10;
 function cambiarValor(a){ // Paso por valor
