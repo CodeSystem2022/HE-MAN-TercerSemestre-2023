9d4e7f22506f3f4602a96544844fe9ad1499b345
Fernando Cala <fernando221298@gmail.com>
2023-05-16 20:11:03 -0300

    1.1 Manejo del forEach Parte 1
    Subo al repositorio

---
+++ JAVA/Leccion03/ForEach/src/main/java/test/TestForEach.java
@@ -0,0 +1,16 @@
+
+package test;
+
+public class TestForEach {
+    public static void main(String[] args) {
+        int edades [] = {5,6,8,9}; //sintaxis resumida
+        for (int edad: edades) { // Sintaxis del ForEach
+            System.out.println("edades = " + edad);
+            
+            
+            
+        }
+        
+    }
+    
+}
