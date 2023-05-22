59e6e82d087f42785b64ceb1a2f4237b7ebdd1bf
fabjav <112377555+fabjav@users.noreply.github.com>
2023-05-21 23:44:38 -0300

    práctica
    práctica con PostgreSQL

--- PYTHON/Lección04/conexión hacia base de datos_python_metodo fetchall.py
+++ PYTHON/Lección04/conexión hacia base de datos_python_metodo fetchall.py
@@ -5,9 +5,22 @@ conexion = psycopg2.connect(
     password = 'admin',
     host = '127.0.0.1',
     port = '5432',
-    database='test_bs'
+    database='test_bd'
 )
-cursor = conexion.cursor()
+cursor = conexion.cursor() #un cursor es un objeto que permite ejecutar sentencias 
 sentencia = 'SELECT * FROM persona'
+sentencia2= 'INSERT INTO persona(nombre,apellido, email) VALUES (%s,%s,%s)'
+datos = ('Jorge', 'Almirón','jorgealmironboquita@mail.com')
+cursor.execute(sentencia2, datos)
+conexion.commit()
 cursor.execute(sentencia) # así ejecutamos la sentencia
+
 registros = cursor.fetchall()#recuperamos los registros que serán una lista
+
+print(registros)
+
+cursor.close()
+conexion.close()
+
+
+
