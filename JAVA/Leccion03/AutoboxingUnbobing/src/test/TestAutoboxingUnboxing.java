1367d70d3f927a73c08ae63e25ed723717f79c3f
fabjav <112377555+fabjav@users.noreply.github.com>
2023-05-22 23:16:07 -0300

    Fabio Javier Flores_AutoboxingUnboxingP1
    parte 1

---
+++ JAVA/Leccion03/AutoboxingUnbobing/src/test/TestAutoboxingUnboxing.java
@@ -0,0 +1,21 @@
+
+package test;
+
+
+public class TestAutoboxingUnboxing {
+    public static void main(String[] args){
+        //clases envolventes o Wrapper
+        /*
+            Clase envolventes de tipos primitivos 
+            int = la clase envolvente es Integer
+        
+        */
+        int enteroPrim = 10; //Tipo primitivo
+        System.out.println("enteroPrim = " + enteroPrim);
+        Integer entero = 10; //Tipo object con la clase Intiger
+        System.out.println("entero = " + entero.toString());
+        
+     
+    }
+    
+}
