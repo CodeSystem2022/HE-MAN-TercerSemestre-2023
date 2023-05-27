ee6515a50302cb7d8ac6581a31178820704ebeb5
MatiasMichaux <112663570+MatiasMichaux98@users.noreply.github.com>
2023-05-27 00:48:36 -0300

    clases de javascript
    5.4 Distintas formas de crear un objeto     5.5 Uso de Prototipe

---
+++ JAVASCRIPT/Leccion05/5.4Distintas-formas-de-crear-un-objeto.js
@@ -0,0 +1,30 @@
+//diferentes formas de crear objetos 
+//caso 1 
+let miObjeto = new Object(); //forma formal
+//caso 2 
+let miObjeto2 = {}; // esta forma es breve y recomendada 
+
+//caso String 1 
+let miCadena = new String("hola ");//sintaxis formal 
+//caso String 2 
+let miCadena2 = "hola "; // esta es la manera simplificada y recomendada 
+
+//caso con numeros 1 
+let miNumero = new Number(1);
+//caso con numero 2 
+let miNumero2 = 1;
+
+//caso boolean 
+let miBoolean = new Boolean(false);
+//case boolean 2
+let miBoolean2 = (false);
+
+//caso arreglos 
+let miArreglo = new Array();
+//caso dos 
+let miArreglo2 =[] //sintaxis recomenda 
+
+//caso funcion 
+let mifuncion1 = new function(){};
+//caso dos 
+let mifuncion2 = function(){};
---
+++ JAVASCRIPT/Leccion05/5.5Uso-de-prototipe.js
@@ -0,0 +1,7 @@
+//uso de prototype // agregar un atributos a todos los objetos 
+persona3.prototype.telefono = "2543567573";
+console.log(padre);
+console.log(madre.telefono);
+madre.telefono = "342423324";
+console.log(madre.telefono);
+
