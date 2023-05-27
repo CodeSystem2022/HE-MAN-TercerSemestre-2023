be14029fe84160f8ecb334479445d95a9cef4e8b
1juba <112666824+1juba@users.noreply.github.com>
2023-05-26 22:18:24 -0300

    5.4-Insertar un registro con psicopg2
    Insertar un registro con psicopg2

--- PYTHON/Leccion05/insertar_reg_psicopg2.py
+++ PYTHON/Leccion05/insertar_reg_psicopg2.py
@@ -10,11 +10,11 @@ try:
                 ('Carlos', 'Lara', 'clara@mail.com'),
                 ('Marcos', 'Canto', 'mcanto@mail.com'),
                 ('Marcelo', 'Cunca', 'cuencaM@mail.com')
-            )
-            cursor.executemany(sentencia, valores)  # ejecutamos la conexión y actualizamos la base de datos
+            ) # Es una tupla de tuplas
+            cursor.executemany(sentencia, valores)  # de esta manera ejecutamos la sentencia
+            # conexion.commit() esto se utiliza para guardae los cambios en la base de dato
             registros_insertados = cursor.rowcount
             print(f'Los registros insertados son: {registros_insertados}')
-
 
 except Exception as e:
     print(f'Ocurrió un error: {e}')
