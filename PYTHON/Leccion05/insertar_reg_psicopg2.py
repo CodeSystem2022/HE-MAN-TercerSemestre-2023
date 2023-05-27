598d0987f46dd160bb68b01dff6512023bcc6b8d
1juba <112666824+1juba@users.noreply.github.com>
2023-05-26 23:10:42 -0300

    5.1-Uso de with y psicopg2
    Uso de with y psicopg2

--- PYTHON/Leccion05/insertar_reg_psicopg2.py
+++ PYTHON/Leccion05/insertar_reg_psicopg2.py
@@ -1,7 +1,6 @@
 import psycopg2  # Conexión base de datos
 
 conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
-
 try:
     with conexion:
         with conexion.cursor() as cursor:
@@ -10,9 +9,8 @@ try:
                 ('Carlos', 'Lara', 'clara@mail.com'),
                 ('Marcos', 'Canto', 'mcanto@mail.com'),
                 ('Marcelo', 'Cunca', 'cuencaM@mail.com')
-            ) # Es una tupla de tuplas
-            cursor.executemany(sentencia, valores)  # de esta manera ejecutamos la sentencia
-            # conexion.commit() esto se utiliza para guardae los cambios en la base de dato
+            )
+            cursor.executemany(sentencia, valores)  # Guardamos los cambios en la base de datos
             registros_insertados = cursor.rowcount
             print(f'Los registros insertados son: {registros_insertados}')
 
