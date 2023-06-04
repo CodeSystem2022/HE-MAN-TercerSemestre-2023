d4483016ae36e2eda7259773b667b58d4bb88b30
Fernando Cala <fernando221298@gmail.com>
2023-06-03 22:36:21 -0300

    TestInterfaces

---
+++ JAVA/Leccion06/Interfaces/src/main/java/test/Testinterfaces.java
@@ -0,0 +1,12 @@
+
+package test;
+
+public class Testinterfaces {
+    public static void main(String[] args) {
+        IAccesoDatos datos = new ImplementacionMySql();
+        
+        
+        
+    }
+    
+}
