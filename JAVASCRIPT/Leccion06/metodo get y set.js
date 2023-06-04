99ac40fb9eec2546fb4b2072aca24c1313b15799
EMMANUEL-1985 <112596295+Emmanuel-1985@users.noreply.github.com>
2023-06-04 19:46:09 -0300

    7.3 Clase Object, toString, sobreescritura y Polimorfismo
    7.3 Clase Object, toString, sobreescritura y Polimorfismo en JavaScript

--- JAVASCRIPT/Leccion06/metodo get y set.js
+++ JAVASCRIPT/Leccion06/metodo get y set.js
@@ -1,68 +1,5 @@
-class Persona{ //Clase padre
-
-    static contadorPersonas = 0; // Atributo estatico
-    //email = "Valor default email";  //Atributo no estatico
-
-    static get MAX_OBJ(){  //Este metodo simula una constante
-        return 5;
-    }
-
-    constructor(nombre, apellido){
-        this._nombre = nombre;
-        this._apellido = apellido;
-        if(Persona.contadorPersonas < Persona.MAX_OBJ){
-            this.idPersona = ++Persona.contadorPersonas;
-        }
-        else{
-            console.log("Se ha superado el maximo de objetos permitidos");
-        }
-        //console.log("Se incrementa el contador: "+Persona.contadorObjetosPersona);
-    }
-    get nombre(){
-        return this._nombre;
-    }
-    set nombre(nombre){
-        this._nombre = nombre;
-    }
-    get apellido(){
-        return this._apellido;
-    }
-    set apellido(apellido){
-        this._apellido = apellido;
-    }
-
-    nombreCompleto(){
-        return this.idPersona+" "+this._nombre+" "+this._apellido;
-    }
-    // Sobreescribiedo el metodo de la clase padre(Object)
-    toString(){ //Regresa un String
-        //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecucion
-        //El metodo que se ejecuta depende si es una refrencia de tipo padre o hija
-        return this.nombreCompleto();
-    }
-
-    static saludar(){
-        console.log("Saludos desde el metodo static");
-    }
-
-    static saludar2(persona){
-        console.log(persona.nombre+" "+persona.apellido);
-    }
-}
-
-class Empleado extends Persona{ //Clase hija
-    constructor(nombre, apellido,departamento){
-        super(nombre, apellido);
-        this._departamento = departamento;
-    }
-
-    get departamento(){
-        return this._departamento;
-    }
-
-    set departamento(departamento){
-        this._departamento = departamento;
-    }
+//let persona3 = new Persona("Carla", "Ponce");  //esto no se debe hacer: Persona is not defined
+    
 class Persona{// CLASE PADRE
     constructor(nombre, apellido){
         this._nombre = nombre;
@@ -87,7 +24,12 @@ class Persona{// CLASE PADRE
     nombrecompleto(){
         return this._nombre+', '+this._apellido;
     }
-
+    // Sobreescribiedo el metodo de la clase padre(Object)
+    toString(){ //Regresa un String
+        //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecucion
+        //El metodo que se ejecuta depende si es una refrencia de tipo padre o hija
+        return this.nombreCompleto();
+    }
 }
 
 class Empleado extends Persona{
@@ -136,3 +78,7 @@ console.log(empleado1.nombre)
 console.log(empleado1.apellido)
 console.log(empleado1.departamento)
 console.log(persona1.nombrecompleto());
+
+//Object.prototype.toString  // Esta es la manera de acceder a atributos y metodos de manera dinamica
+console.log(empleado1.toString());
+console.log(persona1.toString());
