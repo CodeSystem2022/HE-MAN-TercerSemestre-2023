1b4b6c6f3dbb89d5ca6625f20e215e692c8a2440
Fernando Cala <fernando221298@gmail.com>
2023-06-03 22:41:37 -0300

    7.5 Pruebas: Parte 1 y 2

--- JAVA/Leccion06/Interfaces/src/main/java/test/Testinterfaces.java
+++ JAVA/Leccion06/Interfaces/src/main/java/test/Testinterfaces.java
@@ -4,7 +4,16 @@ package test;
 public class Testinterfaces {
     public static void main(String[] args) {
         IAccesoDatos datos = new ImplementacionMySql();
-        
+        //datos.listar();
+        //imprimir(datos);
+        datos = new ImplementacionOracle();
+        //datos.listar();
+        imprimir(datos);
+     
+    }
+    
+    public static void imprimir(IAccesoDatos datos) {
+        datos.listar();
         
         
     }
