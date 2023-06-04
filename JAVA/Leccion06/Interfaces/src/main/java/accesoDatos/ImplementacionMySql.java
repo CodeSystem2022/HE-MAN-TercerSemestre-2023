d9820cc890fa3c4a2a5648f7de1769bba7eb5198
fabjav <112377555+fabjav@users.noreply.github.com>
2023-06-04 19:26:58 -0300

    elimino lineas de codigo

--- JAVA/Leccion06/Interfaces/src/main/java/accesoDatos/ImplementacionMySql.java
+++ JAVA/Leccion06/Interfaces/src/main/java/accesoDatos/ImplementacionMySql.java
@@ -9,19 +9,19 @@ public class ImplementacionMySql implements IAccesosDatos {
     
     @Override
     public void insertar(){
-        throw new UnsopportedOperaionException("Not suported yet."); 
+        
     }
     @Override
     public void listar(){
-        throw new UnsopportedOperaionException("Not suported yet."); 
+       
     } 
     @Override
     public void actualizar(){
-        throw new UnsopportedOperaionException("Not suported yet."); 
+       
     }
     @Override
     public void eliminar(){
-        throw new UnsopportedOperaionException("Not suported yet."); 
+        
     }
     
 }
