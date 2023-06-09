3931607cb6e029fa8ecd8345d83f74c54f92a13e
1juba <112666824+1juba@users.noreply.github.com>
2023-06-09 20:08:04 -0300

    Excepción_aritmética
    Excepcion_operecion_excepcion

--- JAVA/Leccion08/Excepciones/Aritmetica.java
+++ JAVA/Leccion08/Excepciones/Aritmetica.java
@@ -5,7 +5,7 @@ import excepciones.OperacionExcepcion;
 
 public class Aritmetica {
     public static int division(int numerador, int denominador) throws OperacionExcepcion{
-        if(denominador == 0){
+        if(denominador == 0){// Si el denominador es igual a cero, se lanza una excepción personalizada llamada "OperacionExcepcion" con el mensaje "División por cero"
             throw new OperacionExcepcion("Divisíon por cero");
         }
         return  numerador/denominador;
