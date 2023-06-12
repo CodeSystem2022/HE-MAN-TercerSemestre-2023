afd9ebac9184aa20cbee2d69c3993d7cd6c48b5c
Luciano omar cortez <loc200381@gmail.com>
2023-06-12 17:28:42 -0300

    9.5 metodo eliminar
    9.5 Metodo Eliminar

--- PYTHON/Leccion09/persona_dao.py
+++ PYTHON/Leccion09/persona_dao.py
@@ -46,11 +46,25 @@ class PersonaDAo:
                 log.debug(f"Persona actualizada: {persona}")
                 return cursor.rowcount
 
+    @classmethod
+    def eliminar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenercursor() as cursor:
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f"Persona actualizada: {persona}")
+                return cursor.rowcount
+
 if __name__ == "__main__":
+    # Eliminar un registro
+    persona1 = Persona(id_persona=8)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f"Personas Actualizadas: {personas_eliminadas}")
+
     # Actualizar un registro:
-    persona1 = Persona(1, "Juan Jose", "Pena", "jjpena@mail.com")
-    personas_actualizadas = PersonaDAO.actualizar(persona1)
-    log.debug(f"Personas actualizadas: {personas_actualizadas}")
+    #persona1 = Persona(1, "Juan Jose", "Pena", "jjpena@mail.com")
+    #personas_actualizadas = PersonaDAO.actualizar(persona1)
+    #log.debug(f"Personas actualizadas: {personas_actualizadas}")
 
     #Insertar un registro
     # persona1 = Persona(nombre="Omero", apellido="Ramos", email="romero@mail.com")
