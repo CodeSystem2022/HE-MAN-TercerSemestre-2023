63f090352c4c31756e73aa36097007cee00203e3
fabjav <112377555+fabjav@users.noreply.github.com>
2023-06-12 21:21:44 -0300

    get _idEmpleado() }

--- JAVASCRIPT/Leccion09/Empleado.js
+++ JAVASCRIPT/Leccion09/Empleado.js
@@ -1,11 +1,14 @@
 class Empleado extends Persona{
 
     static contadorEmpleado = 0;
-    
+
     constructor(nombre, apellido, edad, sueldo){
         super(nombre, apellido, edad);
         this._idEmpleado = ++Empleado.contadorEmpleado;
         this._sueldo = sueldo;
         
     }
+
+    get _idEmpleado()
+    
 }
