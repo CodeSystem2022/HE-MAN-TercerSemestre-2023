d4d7d89abb90e303df4a1b9d98b56216ec240b4a
Fernando Cala <fernando221298@gmail.com>
2023-06-13 12:23:56 -0300

    Prueba clase cliente
    JS

--- JAVASCRIPT/Leccion09/PruebaPersona.js
+++ JAVASCRIPT/Leccion09/PruebaPersona.js
@@ -7,3 +7,8 @@ console.log(empleado1.toString());
 
 let empleado2 = new Empleado("Jonas", "Torres",30,7000);
 console.log(empleado2.toString());
+
+//Prueba clase cliente
+let cliente1 = new Cliente("Miguel", "Zala",29,new Date());
+console.log(cliente1.toString());
+
