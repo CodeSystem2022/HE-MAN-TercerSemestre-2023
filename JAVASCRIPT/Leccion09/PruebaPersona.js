edc3dc5e0fbb91c0e2d53e1d59e8529beaa3d783
Fernando Cala <fernando221298@gmail.com>
2023-06-13 12:21:33 -0300

    9.7 Prueba Clase Empleado
    Codigo JS

--- JAVASCRIPT/Leccion09/PruebaPersona.js
+++ JAVASCRIPT/Leccion09/PruebaPersona.js
@@ -2,6 +2,8 @@
 
 
 //Prueba clase empleado
-Let empleado1 = new Empleado("Pedro", "Roman",18,5000);
-console.log(empleado1.toString();)
+let empleado1 = new Empleado("Pedro", "Roman",18,5000);
+console.log(empleado1.toString());
 
+let empleado2 = new Empleado("Jonas", "Torres",30,7000);
+console.log(empleado2.toString());
