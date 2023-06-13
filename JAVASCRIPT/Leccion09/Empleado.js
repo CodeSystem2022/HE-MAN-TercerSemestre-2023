1e49d097df07a5e2159140aee4d41fc5981dd6a7
fabjav <112377555+fabjav@users.noreply.github.com>
2023-06-12 21:28:33 -0300

    9.3 Creación de la Clase Empleado

--- JAVASCRIPT/Leccion09/Empleado.js
+++ JAVASCRIPT/Leccion09/Empleado.js
@@ -18,7 +18,10 @@ class Empleado extends Persona{
     }
 
     toString(){
-        return super.toString()+' '+this._idEmpleado+' '+this._sueldo;
+        return `
+        ${super.toString()} 
+        ${this._idEmpleado} 
+        ${this._sueldo}`;
     }
 
 }
