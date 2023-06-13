aa2837c6b843301f1f633fe30ded609e08caeed0
Mauricio Cerda <MauryCerda@users.github.com>
2023-06-13 20:48:52 -0300

    9.2_definiendo_variables
    semana09-cerda

---
+++ JAVA/Leccion09/CalculadoraUTN/9.2_Definiendo_las_variables.java
@@ -0,0 +1,11 @@
+public class CalculadoraUTN {
+    public static void main(String[] args){
+
+        System.out.println("***** Aplicacion Calculadora *****");
+        var operando1 = 10;
+        var operando2 = 10;
+        var resultado = operando1 + operando2;
+        System.out.println("Resultado = " + resultado);
+    }
+
+}
