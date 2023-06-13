076358b5595d958cd31c37b0cc0bb0aa88887689
Fernando Cala <fernando221298@gmail.com>
2023-06-13 12:29:09 -0300

    9.8 Prueba Clase Cliente
    Subo código JS

--- JAVASCRIPT/Leccion09/PruebaPersona.js
+++ JAVASCRIPT/Leccion09/PruebaPersona.js
@@ -12,3 +12,6 @@ console.log(empleado2.toString());
 let cliente1 = new Cliente("Miguel", "Zala",29,new Date());
 console.log(cliente1.toString());
 
+let cliente2 = new Cliente("Natalia", "Ortega",22,new Date());
+console.log(cliente2.toString());
+
