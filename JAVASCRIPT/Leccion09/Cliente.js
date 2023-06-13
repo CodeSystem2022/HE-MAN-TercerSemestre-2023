33d8af881f25d9ddce8dac86af28d4cac5ee611d
fabjav <112377555+fabjav@users.noreply.github.com>
2023-06-12 21:40:10 -0300

    constructor

--- JAVASCRIPT/Leccion09/Cliente.js
+++ JAVASCRIPT/Leccion09/Cliente.js
@@ -1,4 +1,12 @@
 class Cliente extends Persona{
     static contadorClientes = 0;
 
- 
+    constructor(nombre,apellido,edad,fecharegistro){
+        super(nombre,apellido,edad);
+
+        this._idCliente = ++Cliente.contadorClientes;
+        this._fechaRegistro = fecharegistro;
+    }
+
+   
+}
