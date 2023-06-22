82ce07b4e5ae3912576397e3ec3eba487bbf6f0a
fabjav <112377555+fabjav@users.noreply.github.com>
2023-06-22 19:28:30 -0300

    cambios

--- JAVA/Leccion10/ListarPersonas/src/ListadoPersonasApp.java
+++ JAVA/Leccion10/ListarPersonas/src/ListadoPersonasApp.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class ListadoPersonasApp {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        //Definimos la lista fuera del ciclo while
         List<Persona> personas = new ArrayList<>();
-
-
+        //Empezamos con el menú
+        var salir = false;
+        while(!salir){
+            mostrarMenu();
         }
-    }
+
+
+
+    }//Fin método main
+}
