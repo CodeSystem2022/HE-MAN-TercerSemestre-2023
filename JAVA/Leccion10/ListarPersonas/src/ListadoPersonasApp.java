69153b693eb96ed12b8e313f14e4f261813e3ae1
fabjav <112377555+fabjav@users.noreply.github.com>
2023-06-22 19:18:39 -0300

    scaner y list

---
+++ JAVA/Leccion10/ListarPersonas/src/ListadoPersonasApp.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListadoPersonasApp {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        List<Persona> personas = new ArrayList<>();
+
+
+        }
+    }
