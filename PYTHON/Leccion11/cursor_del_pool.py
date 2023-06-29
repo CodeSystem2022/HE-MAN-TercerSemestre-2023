4687c9eddc383228aec8dfccbee19197afd9a06f
2022sbm <112724907+2022sbm@users.noreply.github.com>
2023-06-29 19:19:44 -0300

    11.3 Creamos la clase cursor del pool Parte 1 y 2
    11.3 Creamos la clase cursor del pool Parte 1 y 2

---
+++ PYTHON/Leccion11/cursor_del_pool.py
@@ -0,0 +1,27 @@
+from logger_base import log
+from conexion impor
+
+class CursorDelPool:
+    def __int__(self):
+        self._conexion = None
+        self._cursor = None
+
+    def __enter__(self):
+        log.debug("Inicio del metodo with y __enter__")
+        self._conexion = Conexion.obtenerConexion()
+        self._cursor = self._conexion.cursor()
+    def __exit__(self, tipo_exception, valor_exception, detalle_exception):
+        return self._cursor
+
+        log.debug("Se ejecuta el metodo __exit__")
+        if valor_exception:
+            self._conexion.rollback()
+            log.debug(f"Ocurrio una exception: {valor_exception}")
+        else:
+            self._conexion.commit()
+            log.debug("Commit de la transaccion")
+        self._cursor.close()
+        Conexion.liberarConexion(self._conexion)
+
+
+
